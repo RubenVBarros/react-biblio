ed5fa50d043fe390c4b2f4c461e7d69330b3e7a8
RubenVBarros <rubenvenancio67@gmail.com>
2022-05-14 15:41:38 +0200

    ajout des routes de base

--- src/Pages/Addbook.js
+++ src/Pages/Addbook.js
@@ -1,9 +1,46 @@
 import './Addbook.css'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+//import { useState } from 'react';
+
+/*function myForm(){
+    const [inputs,setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const bookName = event.target.bookName;
+        const bookAuthor = event.target.bookAuthor;
+        const bookNbPages = event.target.bookNbPages;
+        const bookEdition = event.target.bookEdition;
+        setInputs(values => ({...values,[name]:value}))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+}*/
 
 function Addbook(){
     return(
-        <div>Je veux ajouter des livres</div>
+        <div>
+            <form>
+                <div className='main'>
+                    <div className='container'>
+                    <label>Nom du bouquin</label>
+                        <TextField id="outlined-basic" name="bookName" variant="outlined"/>
+                        <label>Nom de l'auteur</label>
+                        <TextField id="outlined-basic" name="bookAuthor" variant="outlined"/>
+
+                        <label>Nombre de pages</label>
+                        <TextField id="outlined-basic" name="bookNbPages" variant="outlined"/>
+
+                        <label>Maison d'édition</label>
+                        <TextField id="outlined-basic" name="bookEdition" variant="outlined"/>
+
+                        <Button id="btn" variant='text'>Valider</Button>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
 }
-
 export default Addbook;
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,5 @@
 import './Dashboard.css'
+
 function Dashboard(){
     return(
         <div>
